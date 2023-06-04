7c2be91f268a0a81399ef7527f58e30623d3a643
branmeco <branmeco17@gmail.com>
2023-06-03 22:56:56 -0500

    Estructura del proyecto y componentes

---
+++ src/components/PoekemonOption.vue
@@ -0,0 +1,12 @@
+<template>
+  <h3>Pokemon options</h3>
+</template>
+
+<script>
+export default {
+    
+};
+</script>
+
+<style>
+</style>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>¿Quien es este pokemon?</h1>
+  <!--TODO: img-->
+  <pokemonPicture />
+
+  <!--TODO: options-->
+  <pokemonOption />
+</template>
+
+<script>
+import PokemonPicture from "@/components/PokemonPicture.vue";
+import PokemonOption from "@/components/PoekemonOption.vue";
+
+export default {
+  components: {
+    PokemonPicture,
+    PokemonOption,
+  },
+};
+</script>
